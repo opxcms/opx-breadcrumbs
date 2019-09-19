ed39cd1e265cd71d1a08b78020c7f21dbeb6e4af
LozovoyV <lozovoyv@gmail.com>
2019-09-19 15:20:12 +0300

    Add extra

--- src/register.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'provider' => \Modules\Opx\Breadcrumbs\Breadcrumbs::class,
-    'facade' => \Modules\Opx\Breadcrumbs\OpxBreadcrumbs::class,
-    'facade_alias' => 'OpxBreadcrumbs',
-];
